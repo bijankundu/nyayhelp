b1eaff07e370c9121e824c5dd7ae180131318033
Bijan Kundu <bijankundu@gofynd.com>
2024-02-25 01:17:30 +0530

    Add @radix-ui/react-avatar and @radix-ui/react-hover-card dependencies, and implement HoverCard and Avatar components

--- app/forum/page.tsx
+++ app/forum/page.tsx
@@ -1,7 +1,45 @@
-import React from "react";
+import Link from "next/link";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import ForumCard from "@/components/common/forumCard";
 
 const Page = () => {
-  return <div>Forum</div>;
+  return (
+    <section className="py-10 flex flex-col items-center gap-4">
+      <div className="flex flex-col items-center gap-4 max-w-[50vw]">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center">
+          Forum Discussions
+        </h1>
+        <p className="text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400 text-center">
+          Join the discussion.
+        </p>
+
+        <div className="w-full flex gap-5 mb-5">
+          <Input
+            type="text"
+            placeholder="Search questions"
+            className="w-full"
+          />
+          <Button>Search</Button>
+        </div>
+
+        <div className="w-full flex flex-col items-center gap-5">
+          {[...Array(6)].map((_, i) => (
+            <div key={i}>
+              <ForumCard />
+            </div>
+          ))}
+        </div>
+
+        <div className="mt-10">
+          <Link href="#">
+            <Button>Load More</Button>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Page;
--- components/common/forumCard/index.tsx
+++ components/common/forumCard/index.tsx
@@ -1,31 +1,47 @@
 import Link from "next/link";
-import { MessageSquareText, ThumbsUp } from "lucide-react";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import { MessageSquareText, ThumbsUp, ThumbsDown } from "lucide-react";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+dayjs.extend(relativeTime);
 
 const ForumCard = () => {
   return (
-    <div className="border rounded-lg p-4">
-      <h2 className="text-xl font-bold">What is contract law?</h2>
-      <p className="text-gray-600">
-        Contract law is a body of law that governs, enforces, and interprets
-        agreements related to an exchange of goods, services, properties, or
-        money.
-      </p>
-      <div className="flex items-center justify-between mt-4">
-        <Link className="text-blue-500 hover:underline" href="#">
-          Read More
-        </Link>
-        <div className="flex gap-5">
-          <div className="text-gray-600 flex gap-1 text-sm items-center">
-            <ThumbsUp size={"18px"} />
-            <span>3</span>
+    <Link href={`/forum/test-slug`}>
+      <div className="border rounded-lg p-4">
+        <h2 className="text-xl font-bold">What is contract law?</h2>
+        <p className="text-gray-600">
+          Contract law is a body of law that governs, enforces, and interprets
+          agreements related to an exchange of goods, services, properties, or
+          money.
+        </p>
+        <div className="flex items-center justify-between mt-4">
+          <div className="flex gap-2 items-center text-sm text-gray-500">
+            <Avatar className="h-8 w-8">
+              <AvatarImage src="https://i.pravatar.cc/300" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <p>{dayjs("2024-02-24T16:25:12.221Z").fromNow()}</p>
           </div>
-          <div className="text-gray-600 flex gap-1 text-sm items-center">
-            <MessageSquareText size={"18px"} />
-            <span>13</span>
+          <div className="flex gap-5">
+            <div className="text-gray-600 flex gap-1 text-sm items-center">
+              <ThumbsUp size={"18px"} />
+              <span>30</span>
+            </div>
+            <div className="text-gray-600 flex gap-1 text-sm items-center">
+              <ThumbsDown size={"18px"} />
+              <span>20</span>
+            </div>
+            <div className="text-gray-600 flex gap-1 text-sm items-center">
+              <MessageSquareText size={"18px"} />
+              <span>13</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
