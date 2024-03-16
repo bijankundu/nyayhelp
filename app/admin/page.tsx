756b53ef60b42369dc7244a80a74f0375a637fea
Bijan Kundu <bijankundu@gofynd.com>
2024-03-17 01:08:02 +0530

    Refactor dynamic import in admin page component

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import dynamic from "next/dynamic";
 
-const AddPostEditor = dynamic(import("@/components/admin/addPostEditor"), {
-  ssr: false,
-});
+const AddPostEditor = dynamic(
+  () => import("@/components/admin/addPostEditor"),
+  {
+    ssr: false,
+  }
+);
 
 const Page = () => {
   return (
